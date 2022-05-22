4cae38ad109fd95fdb7ed023ae232c566e857cca
Harry Cave <46034466+HarryJSCave@users.noreply.github.com>
2022-05-22 17:47:31 +0100

    ya boi is working

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html") 
+    return render_template("Home.html") 
 
 @app.route('/Question/<cat>')
 def question(cat):
-    return render_template("question.html", cat = cat)
+    return render_template("Question.html", cat = cat)
 
 @app.route('/Question/responce')
 def responce():
-    return render_template("responce.html")
+    return render_template("Responce.html")
     
 if __name__ == '__main__': 
     app.run(debug=True)
