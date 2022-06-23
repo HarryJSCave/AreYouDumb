59fa5beb2308c0dd27bed39f41010be4e4fd08f0
Harry Cave <46034466+HarryJSCave@users.noreply.github.com>
2022-06-23 10:12:17 +0100

    bign bong

--- SQL/UserResponses.sql
+++ SQL/UserResponses.sql
@@ -1,9 +1,11 @@
-
+DROP TABLE user_responses;
 CREATE TABLE user_responses(
+    ResponseID INT NOT NULL AUTO_INCREMENT, 
 	UserID TEXT NOT NULL,
     QuestionID TEXT NOT NULL,
 	Answer TEXT NOT NULL,
     TimeTaken TIME NOT NULL,
 	DateTaken DATE NOT NULL,
-    Correct bit NOT NULL
+    Correct bit NOT NULL,
+    PRIMARY KEY (ResponseID)
 );
